15874911a402db92ef7733eafa452761ba248d5b
Billy Editiano <bllyanos@gmail.com>
2025-07-26 13:13:57 +0700

    feat: initial laravel project

---
+++ lvl/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Quotes</title>
+</head>
+
+<body>
+    <h1>
+        Quotes
+    </h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+    </p>
+</body>
+
+</html>
